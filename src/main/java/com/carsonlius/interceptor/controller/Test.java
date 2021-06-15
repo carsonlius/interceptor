877575ad86b45f901b415cf7551ea03669c30a5e
liusen <liusen@huice.com>
2021-06-15 18:45:19 +0800

    添加log4j2扩展

--- src/main/java/com/carsonlius/interceptor/controller/Test.java
+++ src/main/java/com/carsonlius/interceptor/controller/Test.java
@@ -1,6 +1,5 @@
 package com.carsonlius.interceptor.controller;
 
-import ch.qos.logback.core.util.Loader;
 import com.carsonlius.interceptor.entity.User;
 import com.carsonlius.interceptor.mapper.UserMapper;
 import com.carsonlius.interceptor.service.Consume;
@@ -8,6 +7,7 @@ import com.carsonlius.interceptor.service.UserManager;
 import com.carsonlius.interceptor.service.impl.ConsumeImpl;
 import com.carsonlius.interceptor.service.impl.LogHandler;
 import com.carsonlius.interceptor.service.impl.UserManagerImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Repository;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 import java.util.function.Supplier;
 
-
+@Slf4j
 @RestController
 public class Test {
     @Autowired
